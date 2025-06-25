4af3801989c1373d9188a14e797c50d32268bf71
julzedz <edoziechukwuma@gmail.com>
2025-06-25 16:43:35 +0100

    Update property and user models with associations, and enums.

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,9 @@
 class Property < ApplicationRecord
+  enum purpose: { rent: 0, sale: 1 }
+  enum property_type: { house: 0, land: 1 }
   belongs_to :user
   belongs_to :locality
+  has_one_attached :picture
   has_many :property_features, dependent: :destroy
   has_many :features, through: :property_features
 end
