61030462898284574d1bcb128620c7d267f4204d
julzedz <edoziechukwuma@gmail.com>
2025-08-29 11:32:58 +0100

    Update property_serializer to include contact name and phone

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -1,6 +1,6 @@
 class PropertySerializer
   include JSONAPI::Serializer
-  attributes :id, :title, :purpose, :street, :property_type, :price, :description, :bedrooms, :bathrooms, :instagram_video_link, :created_at
+  attributes :id, :title, :purpose, :street, :property_type, :price, :description, :contact_name, :contact_phone, :bedrooms, :bathrooms, :instagram_video_link, :created_at
 
   belongs_to :user
   belongs_to :locality
