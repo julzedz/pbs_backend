04267c57361d02f9b02782f9b53f3f2f3eb736ae
julzedz <edoziechukwuma@gmail.com>
2025-09-08 09:08:55 +0100

    Configure Puma for production

--- config/puma.rb
+++ config/puma.rb
@@ -23,7 +23,10 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+#port ENV.fetch("PORT") { 3000 } #commented out for production
+
+# Specifies the `bind` to listen on the Puma Unix socket
+bind "unix:///home/deploy/pbs_backend/shared/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
