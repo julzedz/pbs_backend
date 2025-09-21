7f1a5c358a39d3b399aa6c9dd00ebeeada81db16
julzedz <edoziechukwuma@gmail.com>
2025-09-21 23:11:02 +0100

    Implement add & remove functionality in FeaturedProperty admin for property selection.

--- app/admin/featured_properties.rb
+++ app/admin/featured_properties.rb
@@ -13,20 +13,106 @@ ActiveAdmin.register FeaturedProperty do
       FeaturedProperty.first_or_create
     end
 
+    def add_property
+      featured = FeaturedProperty.first_or_create
+      property_id = params[:property_id]
+      
+      if property_id.present? && !featured.property_ids.include?(property_id.to_i)
+        featured.property_ids << property_id.to_i
+        featured.save
+        flash[:notice] = "Property added to featured list"
+      elsif featured.property_ids.include?(property_id.to_i)
+        flash[:alert] = "Property is already featured"
+      else
+        flash[:alert] = "Invalid property"
+      end
+      
+      redirect_to edit_admin_featured_property_path(featured)
+    end
+
+    def remove_property
+      featured = FeaturedProperty.first_or_create
+      property_id = params[:property_id]
+      
+      if property_id.present? && featured.property_ids.include?(property_id.to_i)
+        featured.property_ids.delete(property_id.to_i)
+        featured.save
+        flash[:notice] = "Property removed from featured list"
+      else
+        flash[:alert] = "Property not found in featured list"
+      end
+      
+      redirect_to edit_admin_featured_property_path(featured)
+    end
+  end
+
+  member_action :add_property, method: :post do
+    add_property
+  end
+
+  member_action :remove_property, method: :delete do
+    remove_property
   end
 
   form do |f|
-    f.inputs do
-      f.input :property_ids,
-      as: :select,
-      multiple: true,
-      collection: Property.all.map { |p| ["#{p.title} (ID: #{p.id})", p.id] },
-      input_html: { class: "select2" }
+    f.inputs "Add New Property to Featured List" do
+      div class: 'field' do
+        label "Select Property to Add"
+        select_tag :new_property_id, 
+                   options_from_collection_for_select(
+                     Property.where.not(id: f.object.property_ids), 
+                     :id, 
+                     :title
+                   ),
+                   { prompt: "Choose a property to add", class: "select2" }
+        
+        div do
+          link_to "Add Property", 
+                  "#", 
+                  class: "button add-property-btn",
+                  onclick: "addPropertyToFeatured()"
+        end
+        
+        javascript_tag do
+          <<~JS
+            function addPropertyToFeatured() {
+              const selectElement = document.getElementById('new_property_id');
+              const propertyId = selectElement.value;
+              
+              if (!propertyId) {
+                alert('Please select a property first');
+                return;
+              }
+              
+              if (confirm('Add this property to featured list?')) {
+                const form = document.createElement('form');
+                form.method = 'POST';
+                form.action = '#{add_property_admin_featured_property_path(f.object)}?property_id=' + propertyId;
+                
+                const methodInput = document.createElement('input');
+                methodInput.type = 'hidden';
+                methodInput.name = '_method';
+                methodInput.value = 'POST';
+                form.appendChild(methodInput);
+                
+                const tokenInput = document.createElement('input');
+                tokenInput.type = 'hidden';
+                tokenInput.name = 'authenticity_token';
+                tokenInput.value = document.querySelector('meta[name="csrf-token"]').content;
+                form.appendChild(tokenInput);
+                
+                document.body.appendChild(form);
+                form.submit();
+              }
+            }
+          JS
+        end
+      end
+    end
 
-      # This is the list of properties that are currently featured.
-      div id: 'selected_properties_list', class: 'border-2 border-dashed border-gray-300 p-4 rounded-lg' do
+    f.inputs "Currently Featured Properties" do
       if f.object.property_ids.present?
-        table do
+        table class: 'index_table' do
           thead do
             tr do
               th "Title"
@@ -34,6 +120,7 @@ ActiveAdmin.register FeaturedProperty do
               th "Price"
               th "Purpose"
               th "Type"
+              th "Actions"
             end
           end
           tbody do
@@ -42,18 +129,46 @@ ActiveAdmin.register FeaturedProperty do
                 td property.title
                 td property.id
                 td number_to_currency(property.price)
-                td property.purpose
-                td property.property_type
+                td property.purpose.humanize
+                td property.property_type.humanize
+                td do
+                  link_to "Remove", 
+                          remove_property_admin_featured_property_path(f.object, property_id: property.id), 
+                          method: :delete,
+                          class: "button",
+                          confirm: "Remove this property from featured list?",
+                          style: "background-color: #dc3545; color: white; padding: 5px 10px; border-radius: 3px; text-decoration: none;"
+                end
               end
             end
           end
         end
       else
-        text_node 'No featured properties selected.'
+        div class: 'no-content' do
+          text_node 'No featured properties selected.'
+        end
       end
     end
-    end
+
+    # Hidden field to maintain the current property_ids
+    f.input :property_ids, as: :hidden, value: f.object.property_ids.join(',')
 
     f.actions
+  end
+
+  show do
+    attributes_table do
+      row "Featured Properties" do |featured|
+        if featured.property_ids.present?
+          ul do
+            Property.where(id: featured.property_ids).each do |property|
+              li "#{property.title} (ID: #{property.id}) - #{number_to_currency(property.price)}"
+            end
+          end
+        else
+          "No featured properties"
+        end
+      end
+    end
   end
 end
